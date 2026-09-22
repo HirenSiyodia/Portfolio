53cca38e2ec3ccd976f7881d9af9e76df321e402
Hiren Siyodia <siyodiahiren7@gmail.com>
2026-09-22 18:04:39 +0530

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="fixed top-0 left-0 w-full z-50 bg-black/50">
+<nav className="fixed top-0 left-0 w-full z-50 bg-black/90 backdrop-blur-md border-b border-gray-800/70">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           {/* Main Navbar */}
           <div className="flex items-center justify-between h-17">
